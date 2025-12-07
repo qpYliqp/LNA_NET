b2d97d0933c6948e7f91981e9b5a272256e99a24
Yli <yliane.villot@hotmail.fr>
2025-12-07 01:02:19 +0100

    Migrations status

---
+++ src/Data/Migrations/20251206223842_init status.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Status");
+        }
+    }
+}
---
+++ src/Data/Migrations/20251206224905_seed status data.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedstatusdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Data.Seeds.StatusInitData.sql";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new FileNotFoundException($"Le script SQL '{resourceName}' n'a pas été trouvé comme ressource embarquée.");
+                }
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string sqlScript = reader.ReadToEnd();
+                    migrationBuilder.Sql(sqlScript);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Status",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3, 4 });
+        }
+    }
+}
