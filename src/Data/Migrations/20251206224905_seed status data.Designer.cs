dc30f9d414a0057f348456fb3865483f74fdfe79
Yli <75490693+qpYliqp@users.noreply.github.com>
2025-12-08 17:48:02 +0100

    [LN-7]: Table Status
    * Ajout status + script sql pour mettre les données
    
    * Migrations status
    
    * designer migration files
    
    https://ylianevillot.atlassian.net/jira/software/projects/LN/boards/67?selectedIssue=LN-7

--- src/Data/Migrations/20251206224905_seed status data.Designer.cs
+++ src/Data/Migrations/20251206224905_seed status data.Designer.cs
@@ -2,6 +2,7 @@
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251206224905_seed status data")]
+    partial class seedstatusdata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
