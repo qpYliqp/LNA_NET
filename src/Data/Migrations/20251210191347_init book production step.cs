095723b3d1534ed211b8c72650bc79618d35fb31
Yli <yliane.villot@hotmail.fr>
2025-12-10 20:25:11 +0100

    migrations

---
+++ src/Data/Migrations/20251210191347_init book production step.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initbookproductionstep : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductionStep",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductionStep", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookStep",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    StatusId = table.Column<int>(type: "integer", nullable: false),
+                    BookId = table.Column<int>(type: "integer", nullable: false),
+                    ProductionStepId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStep", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookStep_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookStep_ProductionStep_ProductionStepId",
+                        column: x => x.ProductionStepId,
+                        principalTable: "ProductionStep",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BookStep_Status_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookStep_BookId_ProductionStepId",
+                table: "BookStep",
+                columns: new[] { "BookId", "ProductionStepId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookStep_ProductionStepId",
+                table: "BookStep",
+                column: "ProductionStepId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookStep_StatusId",
+                table: "BookStep",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookStep");
+
+            migrationBuilder.DropTable(
+                name: "ProductionStep");
+        }
+    }
+}
